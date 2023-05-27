2959ccc7464a4dbc9289c56e23735b8e29cb2bc3
xRetry <marcoschwarz.at@gmail.com>
2023-05-27 22:35:13 +0200

    Change group keys to home row

--- keymaps.py
+++ keymaps.py
@@ -56,22 +56,23 @@ def create_keys(mod, terminal, browser, groups):
         Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     ]
 
-    for i in groups:
+    group_keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"]
+    for i, group in enumerate(groups):
         keys.extend(
             [
                 # mod1 + letter of group = switch to group
                 Key(
                     [mod],
-                    i.name,
-                    lazy.group[i.name].toscreen(),
-                    desc="Switch to group {}".format(i.name),
+                    group_keys[i],
+                    lazy.group[group.name].toscreen(),
+                    desc="Switch to group {}".format(group.name),
                 ),
                 # mod1 + shift + letter of group = switch to & move focused window to group
                 Key(
                     [mod, "shift"],
-                    i.name,
-                    lazy.window.togroup(i.name, switch_group=True),
-                    desc="Switch to & move focused window to group {}".format(i.name),
+                    group_keys[i],
+                    lazy.window.togroup(group.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(group.name),
                 ),
                 # Or, use below if you prefer not to switch to that group.
                 # # mod1 + shift + letter of group = move focused window to group
